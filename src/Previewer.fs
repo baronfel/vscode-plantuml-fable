c4a990a7397ebbc40fb012c14d13e54f824c1196
Chet Husk <chusk3@gmail.com>
2019-03-19 22:08:01 -0500

    add fable bindings for vscode/node

--- src/Previewer.fs
+++ src/Previewer.fs
@@ -1,6 +1,17 @@
 namespace VSCode.PlantUML.Fable
 
+open Ionide.VSCode.Helpers
+open Fable.Import
+
 module Previewer = 
 
-    let activate _context = 
+    let renderPreview _ = 
+        printfn "rendering plantuml preview!"
+        unbox ()
+
+    let activate _context =
+        vscode.commands.registerCommand(
+            "plantuml.preview",
+            renderPreview
+        ) |> ignore
         printfn "previewer activated"
